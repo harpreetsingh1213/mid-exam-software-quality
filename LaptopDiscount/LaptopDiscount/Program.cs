36bc8f0bb8f37bbeb70dd752ba7e491b8647909d
Harpreet <harpreetssingh1213@gmail.com>
2024-06-17 15:24:00 -0400

    Added new project and create discount class for Employee discount based on different types

---
+++ LaptopDiscount/LaptopDiscount/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopDiscount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helo");
+        }
+    }
+}
